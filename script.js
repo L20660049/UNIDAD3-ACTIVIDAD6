68fc917a780302c1ad09059e654a73719b5a2513
L20660049 <L20660049@matehuala.tecnm.mx>
2024-10-10 20:37:31 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,13 @@
 function cambiarImagen() {
+    // Obtiene el elemento de imagen por su id "imagen"
     var imagen = document.getElementById("imagen");
+
+    // Verifica si el atributo 'src' de la imagen contiene "imagen1.jpg"
     if (imagen.src.includes("imagen1.jpg")) {
+        // Si es así, cambia la imagen a "imagen2.jpg"
         imagen.src = "imagen2.jpg";
     } else {
+        // Si no, cambia la imagen de vuelta a "imagen1.jpg"
         imagen.src = "imagen1.jpg";
     }
 }
